9d15261306e058703577a2d43286c520afff0a30
Sam Griffen <samgriffen99@gmail.com>
2017-09-25 17:51:37 +1300

    Added rules page

---
+++ includes/pages/rules.php
@@ -0,0 +1,75 @@
+<article id="rules">
+  <header>
+    <h1>Rules and Reminders of Examination</h1>
+  </header>
+  <nav>
+    <h2>Quick Navigation</h2>
+    <ul>
+      <li class="nav-li"><a href="#what-to-bring">What to Bring</a></li>
+      <li class="nav-li"><a href="#procedure">Examination Procedure</a></li>
+      <li class="nav-li"><a href="#sick">What if you are sick, or there is a Family Emergency?</a></li>
+      <li class="nav-li"><a href="#calculators">Approved Calculators</a></li>
+    </ul>
+  </nav>
+  <section>
+    <h2 id="what-to-bring">What to Bring:</h2>
+    <ul>
+      <li>2016 EXAMINATIONS ADMISSION SLIP with YOUR name on.  Do NOT write on your admission slip.
+  If you forget: Print one off the NZQA website through the learner login and then take it to the office to get it signed OR Go to the office. They will give you a signed replacement.</li>
+
+      <li>Scholarship candidates MUST have photo ID (Onslow ID, drivers licence, passport).</li>
+
+      <li>ONLY water is allowed in the exam rooms.</li>
+
+      <li>All stationery must be placed in a clear plastic bag. You cannot borrow during the exam. The office sells stationery. It is forbidden to write in pencil or to use white correction pens or fluid. Coloured pencils or biros are fine on maps, graphs and diagrams but use only blue or black biro for actual writing.</li>
+
+      <li>An APPROVED calculator for an approved subject. Maximum two approved calculators allowed in the exam.
+  The memory of all calculators need to be cleared and shown to the Supervisor.  Ensure you know how to do this BEFORE your exam.  See page 4 of this document for a list of approved calculators.</li>
+    </ul>
+  </section>
+  <section>
+    <h2 id="procedure">Examination Procedure:</h2>
+    <ul>
+      <li>Arrive at school at least 20 minutes before start time. (French Pilot students need to arrive 30 minutes earlier.)
+  Go to the link foyer to check which room you are in. Use the 4 digit exam number on your exam slip e.g. B120.</li>
+
+      <li>If you arrive 31 minutes after the start of the examination you will not be let in and you will get no grade.</li>
+
+      <li>After the first 45 minutes you can leave the exam room but you can’t leave in the last 15 minutes of the exam.
+        (There are different rules for people with exam clashes. See your letter.)</li>
+
+      <li>A toilet is available in the Table Mountain area, by room 56.  You must ask for permission before leaving the exam room. A record is kept of how long you spend in the toilet, and how often you go, because of cheating issues.</li>
+
+      <li>If there is an emergency evacuation you need to go with the supervisor to Table Mountain, and you must remain silent.</li>
+      <li>All bags, jackets and coats to be placed in the front of the room.</li>
+
+      <li>No electronic device may be taken into the exam and no watches may be worn. Language dictionaries and spell checkers are not allowed.  Cell phones switched off and in your school bag.</li>
+
+      <li>Ensure no notes are in your pockets or plastic bag. Don’t write notes on your body or cover your legs with a jacket. DO NOT bring spare refill into the exam.</li>
+
+      <li>CHECK YOUR EXAM PACK contains all the standards you are sitting.</li>
+
+      <li>If you do not wish to attempt a standard DO NOT PUT A MARK ON IT. A totally empty script will not be graded. A script with one letter, doodle or word in it gets a Not Achieved grade.</li>
+
+      <li>Obey supervisors’ instructions and be courteous. You must remain seated at the end until all scripts have been collected. Do not walk out with the papers. Do not look around the room.</li>
+    </ul>
+  </section>
+
+  <section>
+    <h2 id="sick">What if you are sick, or there is a Family Emergency?</h2>
+    <ul>
+      <li>If you are unable to sit exams due to illness, injury, a bereavement and so on, you may apply for a derived grade. These events must be recent, serious and out of your control. A special derived grade form must be collected from the school office or from Esme Danielsen before you go to the doctor or counsellor. All the information has to be returned to Esme Danielsen before 12.00 noon on Dec 8th. There are no derived grades for Scholarship.</li>
+
+      <li>If you sit your exam but you were ill or had to leave half way through see Esme Danielsen. You can apply for a derived grade even though any completed exams papers will also be marked.</li>
+
+      <li>You can only apply for a derived grade for a particular standard if the school has standard specific evidence on file, that is, you have achieved a grade for the practice test.</li>
+    </ul>
+  </section>
+
+  <section>
+    <h2 id="calculators">Approved Calculators</h2>
+    <p>When sitting an NZQA exam, you are only allowed to use a calculator from a list of calculators that NZQA has approved. The link below will point you to this list of calculators that NZQA has approved:</p>
+    <a href="http://www.nzqa.govt.nz/about-us/publications/newsletters-and-circulars/assessment-matters/approved-calculators-list-for-external-assessments-in-2017/">NZQA List of Approved Calculators</a>
+  </section>
+
+</article>
